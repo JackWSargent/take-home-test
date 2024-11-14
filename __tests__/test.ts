4c79e82579429006ddba838a9a9fd78f5c84168a
Jack Sargent <jack@getrealift.com>
2024-11-14 12:13:58 -0500

    Added testing for all-movies

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -1,6 +1,6 @@
 import {beforeAll, describe, expect, test} from '@jest/globals';
 
-describe('Unit Testing', () => {
+describe('Unit Testing for some Movies', () => {
     let results: any = undefined;
     beforeAll(async () => {
         results = await fetch(`http://localhost:3000/api/movies/1998`, {
@@ -16,4 +16,19 @@ describe('Unit Testing', () => {
     test('Expect length of results to be 20', () => {
         expect(results.length).toBe(20);
     })
+});
+
+describe('Unit Testing for all Movies', () => {
+    let results: any = undefined;
+    beforeAll(async () => {
+        results = await fetch(`http://localhost:3000/api/all-movies/1998`, {
+            method: "GET",
+        }).then(res => res.json());
+    }, 30000);
+    test('Expect results to be not null', () => {
+        expect(results).toBeDefined();
+    }, 30000);
+    test('Expect first couple of movies to have a high vote average', () => {
+        expect(results[0].vote_average).toBe(10);
+    }, 30000);
 });
